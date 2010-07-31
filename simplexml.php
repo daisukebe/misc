3e60a299ac5218b218961cae3faa56876a173a72
daisukebe <poleon.kd@gmail.com>
2010-07-31 13:14:39 -0400

    Initial Commit.

---
+++ simplexml.php
@@ -0,0 +1,14 @@
+<?php
+echo 'Hatena Bookmark RSS Test', '<br><br>';
+$url = 'http://b.hatena.ne.jp/daisukebe/favorite.rss';
+$data = simplexml_load_file($url)
+	or die("xml parse error");
+
+foreach($data->item as $item){
+	echo $item->title,
+		' => ',
+		$item->children('hatena', true)->bookmarkcount,
+		'<br>';
+}
+
+
